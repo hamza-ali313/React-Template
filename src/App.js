92ca707d5304bda47b21f09872032707aa955f6c
hamza ali <70458340+hamza-ali313@users.noreply.github.com>
2023-06-03 11:44:37 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,46 +2,12 @@ import { Container, Row, Col, Carousel, Nav, Tab, Sonnet, Button } from 'react-b
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro';
 import React, { useState } from 'react';
-
-// AOS
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
-// SLICK
-import '../node_modules/slick-carousel/slick/slick.css';
-import '../node_modules/slick-carousel/slick/slick-theme.css';
-
-import ScrollToTop from './Views/ScrollToTop/Index';
 import MySlider from './Views/MySlider';
-import clock from "./images/clock.png"
-import screen from "./images/screen.png"
-import calender from "./images/calender.png"
 
-// AOS.init({
-//     offset: 200,
-//     duration: 600,
-//     easing: 'ease-in-sine',
-//     delay: 200,
-// });
-
-
-
-class App extends React.Component {
-   
-    render() {
-        
-        
-        return (
-            
-            <div>
-                <MySlider first={clock} second={screen} third={calender} fourth={clock} fifth={screen}  sixth={calender}/>
-            </div>
-          
-            
-        );
-    }
+const App = () => {
+  return (
+    <div>App</div>
+  )
 }
 
-
-
-export default App;
+export default App
